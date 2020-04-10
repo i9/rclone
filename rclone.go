562eaadba7b2576a64c885fc04ded2d663b6f7cf
Junda Liu <junda@celer.network>
2020-04-09 22:02:37 -0700

    minimal backend and cmd

--- rclone.go
+++ rclone.go
@@ -4,10 +4,11 @@
 package main
 
 import (
-	_ "github.com/rclone/rclone/backend/all" // import all backends
+	_ "github.com/rclone/rclone/backend/amazonclouddrive" // import all backends
+	_ "github.com/rclone/rclone/backend/local"
 	"github.com/rclone/rclone/cmd"
-	_ "github.com/rclone/rclone/cmd/all"    // import all commands
-	_ "github.com/rclone/rclone/lib/plugin" // import plugins
+	_ "github.com/rclone/rclone/cmd/copy"    // import all commands
+	// _ "github.com/rclone/rclone/lib/plugin" // import plugins
 )
 
 func main() {
